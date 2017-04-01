02b8a442abe1c6e137d81302f3b673ad8f932078
Tiago Ferreira Dias <tdias8@hotmail.com>
2017-04-01 18:13:02 -0300

    Criado pagina.html.erb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,10 @@ class PagesController < ApplicationController
   def homepage
   	@ruby = 'variavel'
   end
+
+
+def paginas
+	@pagina = Page.find(params[:id])
+end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |index|
+	Page.create!(
+		title:"Titulo#{index}",
+		body: "Lorem ipsim dolor sit amet")
+end
